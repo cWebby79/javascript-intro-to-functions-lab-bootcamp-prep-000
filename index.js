890b130190a952102a983ba95347b250c77d38f5
Chris Webster <cgw57@hotmail.com>
2018-07-12 16:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!";
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-}
+  }else {
+    return "I love you, too";
+  }
 }
