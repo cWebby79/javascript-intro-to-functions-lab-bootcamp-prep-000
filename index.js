b5d443aa0bc7d425e2696270b73abf832581752f
Chris Webster <cgw57@hotmail.com>
2018-07-12 15:45:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function sayHiToGrandma(string) {
   var mixedCase = 'Hi there!';
   if (lowercase.toLowerCase() === lowercase) {
     return "I can't hear you!";
-  } else if (uppercase.toUpperCase() === uppercase) {
+  }
+  if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!";
   }else {
     return "I love you, too";
