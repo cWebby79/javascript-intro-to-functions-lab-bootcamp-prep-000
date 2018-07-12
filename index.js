21166d4c4bef18c04a9fe2abb6b606dc7f5ae314
Chris Webster <cgw57@hotmail.com>
2018-07-12 16:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string===lowercase.toLowerCase() === lowercase) {
+  if(string.toLowerCase() === lowercase) {
     return "I can't hear you!";
   }
 } 
